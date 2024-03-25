301212a218c2df46dd4f2bb28f65384570ff2eb0
Bob Faist <bob.faist@gmail.com>
2024-03-25 14:43:40 -0400

    add files

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bob Faist on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
